c284b4b67324fa1b8209d167f66ef74d6394bbb4
Andrew Kerr <kerrand@protonmail.com>
2022-03-26 14:47:14 -0400

    update map colouring

--- map.py
+++ map.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
-from collections import defaultdict
+from collections import defaultdict, deque
 from typing import Dict, List
 
-def init_map(fname: str, sort: bool=True) -> Dict[str, List[str]]:
-    '''Initialize a adjacency list representation of a m, reading in
-    territory - territority edges from a given filename. By default, m
+Map = Dict[str, List[str]]
+
+def init_map(fname: str, sort: bool=True) -> Map:
+    '''Initialize a adjacency list representation of a map, reading in
+    territory - territority edges from a given filename. By default, map
     entries are sorted in descending order by neighbour list length.'''
     m = defaultdict(list)
     with open(fname, 'r') as f:
@@ -20,10 +22,33 @@ def init_map(fname: str, sort: bool=True) -> Dict[str, List[str]]:
     else:
         return m
 
+def colour_map(m: Map) -> Dict[str, int]:
+    colours = dict({region: -1 for region in m.keys()})
+    colour_opts = set({'orange', 'green', 'purple', 'yellow'})
+
+    first = tuple(m.keys())[0]
+    visited = set({first})
+    to_visit = deque([first])
+    while len(to_visit) > 0:
+        current = to_visit.popleft()
+        allowed = colour_opts.copy()
+        for neighbour in m[current]:
+            if neighbour not in visited:
+                to_visit.append(neighbour)
+                visited.add(neighbour)
+            allowed -= {colours[neighbour]}
+        colours[current] = tuple(allowed)[0]
+
+    for region, neighbours in m.items():
+        for neighbour in neighbours:
+            assert colours[region] != colours[neighbour]
+
+    return colours
+
 def main():
     usa = init_map('usa_edges.in')
-    print(usa)
-    colours = {state: None for state in usa.keys()}
+    colouring = colour_map(usa)
+    print(colouring)
 
 if __name__ == '__main__':
     main()
