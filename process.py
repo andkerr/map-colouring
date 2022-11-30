4784bf75d1d7359da7cbd3332d646db2545e3c0d
Andrew Kerr <kerrand@protonmail.com>
2022-11-30 09:16:09 -0500

    refactor processing script

--- process.py
+++ process.py
@@ -2,16 +2,15 @@ import subprocess
 import sys
 
 
-def main():
-    if len(sys.argv) != 2:
-        print("usage python3 script.py [edge list file]")
-
+# convert a plaintext edge list with arbitrary vertex identiifers to an
+# edge list containing only integer values
+def standardize_edge_list(fname):
     node_ids = {}
     n_nodes = 0
 
     edges = []
 
-    with open(sys.argv[1], 'r') as f:
+    with open(fname, 'r') as f:
         for line in f:
             if line != '\n':
                 (u, v) = line.split()
@@ -23,11 +22,24 @@ def main():
                     n_nodes += 1
                 edges.append((node_ids[u], node_ids[v]))
 
+    return node_ids, edges
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("usage python3 script.py [edge list file]")
+        return
+
+    node_ids, edges = standardize_edge_list(sys.argv[1])
     colouring_input = '\n'.join([' '.join(map(str, edge)) for edge in edges])
-    p = subprocess.run(['./colouring'],
-                       input=colouring_input,
-                       encoding='ascii',
-                       capture_output=True) 
+    try:
+        p = subprocess.run(['./colouring'],
+                           input=colouring_input,
+                           encoding='ascii',
+                           capture_output=True) 
+    except FileNotFoundError:
+        print('Could not find the colouring executable in this directory')
+        return
 
     node_lookup = {v : k for (k, v) in node_ids.items()}
     colour_strings = ['red', 'green', 'blue', 'yellow']
@@ -35,6 +47,7 @@ def main():
     for line in p.stdout.rstrip().split('\n'):
         (node, colour) = line.split()
         print(node_lookup[int(node)], colour_strings[int(colour)])    
+
 
 if __name__ == '__main__':
     main()
